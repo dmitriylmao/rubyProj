5e288f2f7228bbbd66282485b4e0c46598ecf5ae
dengol04 <dengol04@mail.ru>
2024-12-25 03:40:56 +0300

    added user model tests

---
+++ onlibrary/test/factories/all.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    username { "Олег" }
+    email { "oleg@example.com" }
+    add_attribute(:role) { UsersController::Roles::USER }
+    password_digest {  BCrypt::Password.create("123456") }
+
+    trait :admin do
+      username { "Admin" }
+      email { "admin@example.com" }
+      add_attribute(:role) { UsersController::Roles::ADMIN }
+      password_digest {  BCrypt::Password.create('123456') }
+    end
+  end
+end
