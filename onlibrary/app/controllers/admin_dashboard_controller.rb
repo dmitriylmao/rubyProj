b39b907d8641cf7ae0afa99504df3436583975bd
dengol04 <dengol04@mail.ru>
2025-01-05 17:03:17 +0300

    fixed dashboards access

--- onlibrary/app/controllers/admin_dashboard_controller.rb
+++ onlibrary/app/controllers/admin_dashboard_controller.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class AdminDashboardController < ApplicationController
+  before_action :authenticate_admin
   def index
-     user_id = session[:current_user]
-     user = User.find_by_id(user_id)
-     if !user.present? || User.find_by_id(user_id).role != UsersController::Roles::ADMIN
-       redirect_to "/register"
-     end
+
   end
 end
